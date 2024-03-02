73dab090308e8527a91452877adb3fb20eabf18b
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2024-03-02 16:22:57 -0300

    finalização de alguns detalhes

--- logicaJogo.js
+++ logicaJogo.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * 3);
@@ -5,39 +8,29 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection) {
-  if (playerScore <= 5 && computerScore <= 5) {
-    const computerSelection = getComputerChoice();
-    let result = "";
+  const computerSelection = getComputerChoice();
+  let result = "";
 
-    if (playerSelection === computerSelection) {
-      result = "draw";
-    } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      result = "player";
-    } else {
-      result = "computer";
-    }
-
-    displayResult(result, playerSelection, computerSelection);
-    updateScore(result);
-
-    if (playerScore === 6 || computerScore === 6) {
-      displayWinner();
-      resetGame();
-    }
+  if (playerSelection === computerSelection) {
+    result = "draw";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    result = "player";
+  } else {
+    result = "computer";
   }
+
+  displayResult(result, playerSelection, computerSelection);
+  updateScore(result);
 }
 
 function displayResult(result, playerSelection, computerSelection) {
   const choiceElement = document.getElementById("choice");
   choiceElement.innerHTML = `You chose <span style="color: green">${playerSelection}</span> the computer chose <span style="color: green">${computerSelection}</span>`;
 }
-
-let playerScore = 0;
-let computerScore = 0;
 
 function updateScore(result) {
   const playerScoreElement = document.querySelector(".playerScore");
@@ -48,8 +41,14 @@ function updateScore(result) {
   } else if (result === "computer") {
     computerScore++;
   }
+
   playerScoreElement.textContent = `Player: ${playerScore}`;
   computerScoreElement.textContent = `Computer: ${computerScore}`;
+
+  if (playerScore >= 6 || computerScore >= 6) {
+    displayWinner();
+    resetGame();
+  }
 }
 
 function displayWinner() {
@@ -68,8 +67,8 @@ function resetGame() {
 
   const playerScoreElement = document.querySelector(".playerScore");
   const computerScoreElement = document.querySelector(".computerScore");
-  playerScoreElement.textContent = "Player: 0";
-  computerScoreElement.textContent = "Computer: 0";
+  playerScoreElement.textContent = `Player: ${playerScore}`;
+  computerScoreElement.textContent = `Computer: ${computerScore}`;
 }
 
 function handleClick(selection) {
